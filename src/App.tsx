960af467c941e8e329a2b25cb14fa14d78085b09
pixelperfectdivami <pixelperfect@divami.com>
2024-08-01 14:55:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
   useNavigationType,
   useLocation,
 } from "react-router-dom";
-import undefined from "./";
+import Profile from "./components/Profile";
 
 function App() {
   const action = useNavigationType();
@@ -45,7 +45,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<undefined />} />
+      <Route path="/" element={<Profile />} />
     </Routes>
   );
 }
